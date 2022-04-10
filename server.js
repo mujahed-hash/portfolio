631116b3d74547526fdb6c3e8b8e5e3fba29bff6
mujahed-hash <mujahedahmed786@gmail.com>
2022-04-10 19:22:16 +0530

    v3

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/routing-app'));
-<<<<<<< HEAD
-app.get('*', function (req, res) {
-=======
+
 app.get('/*', function (req, res) {
->>>>>>> 511f8d27f73388992a5eff0f5db42f2f35279117
-  res.sendFile(path.join(__dirname + '/dist/routing-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/routing-app/'));
 });
 app.listen(process.env.PORT || 8080);
